0167fea97afbc4b5da376c05bc02febf750f3864
Jeff Liu <jeff.liubing@gmail.com>
2020-09-16 11:43:02 +1200

    change directory

--- angular/angular-app/src/environments/environment.prod.ts
+++ angular/angular-app/src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost/api/task/',   // need change to domain or IP address
+  apiUrl: 'http://34.87.220.195/api/task/',   // need change to domain or IP address
   auth: {
     clientID: 'hfSCrVCJSNY2QexJamnm4XiDlHIcxNbk',
     domain: 'jeffliu.au.auth0.com', // e.g., you.auth0.com
     audience: 'http://dragonexampleapi',
-    auth0RedirectUri: 'http://localhost:8080/callback', // URL to return to after auth0 login
-    auth0ReturnTo: 'http://localhost:8080', // URL to return to after auth0 logout
+    auth0RedirectUri: 'http://34.87.220.195:8080/callback', // URL to return to after auth0 login
+    auth0ReturnTo: 'http://34.87.220.195:8080', // URL to return to after auth0 logout
     scope: 'openid profile'
   }
 };
